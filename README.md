7af20b26bbb49227bbaec8f128b9e006e1597dae
Dave Lawton <dave@supamu.co.uk>
2016-01-06 15:03:33 +0000

    Get working with APEX 5 modal pages and fix a couple of other minor issues

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # oracle-apex-form-filler
-Use to fill out long winded APEX pages with test data
+
+Use to fill out long winded APEX pages with test data.
+
+# Installation
+
+The extension can be installed directly by using developer mode.
+
+* Download as Zip and unpack to a folder.
+* Go to Chrome Extensions in settings and make sure developer mode is on.
+* Load extracted folder as an unpacked extension.
--- content.js
+++ content.js
@@ -2,10 +2,18 @@
 var $selection;
 var message;
 
-var inputHandler = function (selector) {
-   $selection = $(selector);
+var richTextHandler = function (context) {
+   $selection.add('textarea.rich_text_editor', context);
+   $selection.each(function (i, e) {
+      var label = $("label[for='" + this.id + "']", context).text().trim() || 'iframe ' + i;
+      $(e).parent().find('iframe').contents().find('body').html('<p>Test data for ' + label + '</p>')
+   });
+};
+
+var inputHandler = function (selector, context) {
+   $selection = $(selector, context);
    $selection.each(function () {
-      var val = $("label[for='" + this.id + "']").text().trim() || 'Test Data';
+      var val = $("label[for='" + this.id + "']", context).text().trim() || 'Test Data';
       var maxLength = $(this).attr('maxlength') || 200;
       val = val.substring(0, maxLength - 1);
       $(this).val(val);
@@ -14,32 +22,32 @@ var inputHandler = function (selector) {
 
 chrome.runtime.onMessage.addListener(function (request, sender, response) {
    console.log('Method Received ' + request.method);
+
+   var isDialogOpen = $('.ui-dialog').length > 0;
+   var rootSelector = isDialogOpen ? $('.ui-dialog').find('iframe').contents() : $(document);
+
    switch (request.method) {
    case 'ta':
-      inputHandler('textarea:visible');
+   debugger;
+      inputHandler('textarea:visible', rootSelector);
+      richTextHandler(rootSelector);
       break;
    case 'tf':
-      inputHandler('input[type="text"]:not(.datepicker):visible');
+      inputHandler('input[type="text"]:not(.datepicker):visible', rootSelector);
       break;
    case 'df':
-      $selection = $('.datepicker:visible');
+      $selection = $('.datepicker:visible'), rootSelector;
       $selection.val(moment().format('DD-MMM-YYYY'));
       break;
    case 'rb':
-      $selection = $('.radio_group input[type="radio"][value="' + request.action + '"]:visible');
+      $selection = $('.radio_group input[type="radio"][value="' + request.action + '"]:visible', rootSelector);
       $selection.prop('checked', true);
       break;
-   case 'cb':
-      $selection = $('input[type="checkbox"]:visible');
-      $selection.prop('checked', !$selection.prop("checked"));
-      break;
-   case 'lsl':
-      $selection = $('select:visible option:last-child');
-      $($selection).prop("selected", true);
-      break;
    case 'clear':
-      $selection = $('textarea:visible, input[type="text"]:visible, .datepicker:visible');
+      $selection = $('textarea:visible, input[type="text"]:visible, .datepicker:visible', rootSelector);
       $selection.val('');
+      $selection = $selection.add('iframe', 'textarea.rich_text_editor');
+      $('iframe', 'textarea.rich_text_editor').contents().find('body').html('');
       break;
    default:
       console.log(request.method + ' not supported');
